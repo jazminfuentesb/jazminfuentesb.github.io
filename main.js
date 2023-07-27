987c575c6a1343d09405067723afc24bb371beab
Jazmín Fuentes <140033691+jazminfuentesb@users.noreply.github.com>
2023-07-26 19:45:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 100,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Estudiante de QFB y programadora junior')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
